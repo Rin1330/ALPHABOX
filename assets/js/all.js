b13fdfb69559fa98dfdbe46276ccebcaf37cbbc0
sakimotorin <sakimotorin@gmail.com>
2023-11-02 17:46:27 +0800

    update to whats new section

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,19 +2,27 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs
 import '../../node_modules/swiper/swiper-bundle.min.css'
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('hi');
 
   const swiper = new Swiper('#whatIs', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
     // Navigation arrows
     navigation: {
       prevEl: '.swiper-button-prev',
       nextEl: '.swiper-button-next',
     },
-  
   });
+
+  const newSwiper = new Swiper('#news-swiper', {
+    spaceBetween: 24,
+    mousewheel: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      // hide: false,
+      draggable: true
+    },
+    slidePerView: "auto",
+  })
 
 })
--- main.js
+++ main.js
@@ -2,4 +2,3 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './assets/js/all.js';
-console.log("Hello world!");
